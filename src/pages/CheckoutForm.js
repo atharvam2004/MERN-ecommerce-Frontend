736c9841afe4e2319a6a5ec7184a0c1ae324b042
atharvam2004 <meheratharva49@gmail.com>
2024-10-06 12:41:32 +0530

    razorpay

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -1,98 +1,261 @@
-import React, { useEffect, useState } from "react";
-import {
-  PaymentElement,
-  useStripe,
-  useElements
-} from "@stripe/react-stripe-js";
-import { useSelector } from 'react-redux';
-import { selectCurrentOrder } from "../features/order/orderSlice";
+// import React, { useEffect, useState } from "react";
+// import {
+//   PaymentElement,
+//   useStripe,
+//   useElements,
+// } from "@stripe/react-stripe-js";
+// import { useSelector } from "react-redux";
+// import { selectCurrentOrder } from "../features/order/orderSlice";
 
-export default function CheckoutForm() {
-  const stripe = useStripe();
-  const elements = useElements();
+// export default function RenderRazorpay() {
+//   const stripe = useStripe();
+//   const elements = useElements();
+//   const currentOrder = useSelector(selectCurrentOrder);
+
+//   const [message, setMessage] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (!stripe) {
+//       return;
+//     }
+
+//     const clientSecret = new URLSearchParams(window.location.search).get(
+//       "payment_intent_client_secret"
+//     );
+
+//     if (!clientSecret) {
+//       return;
+//     }
+
+//     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+//       switch (paymentIntent.status) {
+//         case "succeeded":
+//           setMessage("Payment succeeded!");
+//           break;
+//         case "processing":
+//           setMessage("Your payment is processing.");
+//           break;
+//         case "requires_payment_method":
+//           setMessage("Your payment was not successful, please try again.");
+//           break;
+//         default:
+//           setMessage("Something went wrong.");
+//           break;
+//       }
+//     });
+//   }, [stripe]);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (!stripe || !elements) {
+//       // Stripe.js hasn't yet loaded.
+//       // Make sure to disable form submission until Stripe.js has loaded.
+//       return;
+//     }
+
+//     setIsLoading(true);
+
+//     const { error } = await stripe.confirmPayment({
+//       elements,
+//       confirmParams: {
+//         // Make sure to change this to your payment completion page
+//         return_url: `https://mern-ecommerce-lyart.vercel.app/order-success/${currentOrder.id}`,
+//       },
+//     });
+
+//     // This point will only be reached if there is an immediate error when
+//     // confirming the payment. Otherwise, your customer will be redirected to
+//     // your `return_url`. For some payment methods like iDEAL, your customer will
+//     // be redirected to an intermediate site first to authorize the payment, then
+//     // redirected to the `return_url`.
+//     if (error.type === "card_error" || error.type === "validation_error") {
+//       setMessage(error.message);
+//     } else {
+//       setMessage("An unexpected error occurred.");
+//     }
+
+//     setIsLoading(false);
+//   };
+
+//   const paymentElementOptions = {
+//     layout: "tabs",
+//   };
+
+//   return (
+//     <form id="payment-form" onSubmit={handleSubmit}>
+//       <PaymentElement id="payment-element" options={paymentElementOptions} />
+//       <button disabled={isLoading || !stripe || !elements} id="submit">
+//         <span id="button-text">
+//           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+//         </span>
+//       </button>
+//       {/* Show any error or success messages */}
+//       {message && <div id="payment-message">{message}</div>}
+//     </form>
+//   );
+// }
+
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import crypto from 'crypto-js';   
+import { selectCurrentOrder } from "../features/order/orderSlice";
+import { useSelector } from 'react-redux';
+
+// Function to load script and append in DOM tree.
+const loadScript = src => new Promise((resolve) => {
+  const script = document.createElement('script');
+  script.src = src;
+  script.onload = () => {
+    console.log('razorpay loaded successfully');
+    resolve(true);
+  };
+  script.onerror = () => {
+    console.log('error in loading razorpay');
+    resolve(false);
+  };
+  document.body.appendChild(script);
+});
+
+
+const RenderRazorpay = ({
+  orderId,
+  keyId,
+  keySecret,
+  currency,
+  amount,
+}) => {
+  const paymentId = useRef(null);
+  const paymentMethod = useRef(null);
   const currentOrder = useSelector(selectCurrentOrder);
 
-  const [message, setMessage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!stripe) {
-      return;
-    }
-
-    const clientSecret = new URLSearchParams(window.location.search).get(
-      "payment_intent_client_secret"
+  // To load razorpay checkout modal script.
+  const displayRazorpay = async (options) => {
+    const res = await loadScript(
+      'https://checkout.razorpay.com/v1/checkout.js',
     );
 
-    if (!clientSecret) {
+    if (!res) {
+      console.log('Razorpay SDK failed to load. Are you online?');
       return;
     }
+    // All information is loaded in options which we will discuss later.
+    const rzp1 = new window.Razorpay(options);
 
-    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      switch (paymentIntent.status) {
-        case "succeeded":
-          setMessage("Payment succeeded!");
-          break;
-        case "processing":
-          setMessage("Your payment is processing.");
-          break;
-        case "requires_payment_method":
-          setMessage("Your payment was not successful, please try again.");
-          break;
-        default:
-          setMessage("Something went wrong.");
-          break;
-      }
-    });
-  }, [stripe]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!stripe || !elements) {
-      // Stripe.js hasn't yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    setIsLoading(true);
-
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-lyart.vercel.app/order-success/${currentOrder.id}`,
-      },
+    // If you want to retreive the chosen payment method.
+    rzp1.on('payment.submit', (response) => {
+      paymentMethod.current = response.method;
     });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
-      setMessage(error.message);
-    } else {
-      setMessage("An unexpected error occurred.");
-    }
+    // To get payment id in case of failed transaction.
+    rzp1.on('payment.failed', (response) => {
+      paymentId.current = response.error.metadata.payment_id;
+    });
 
-    setIsLoading(false);
+    // to open razorpay checkout modal.
+    rzp1.open();
   };
 
-  const paymentElementOptions = {
-    layout: "tabs"
-  }
 
-  return (
-    <form id="payment-form" onSubmit={handleSubmit}>
-      <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
-      </button>
-      {/* Show any error or success messages */}
-      {message && <div id="payment-message">{message}</div>}
-    </form>
-  );
-}
+  // informing server about payment
+  const handlePayment = async (status, orderDetails = {}) => {
+    try {
+      const response = await fetch(`/payment`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          status,
+          orderDetails,
+        }),
+      });
+
+      const result = await response.json();
+      console.log('Payment handled successfully:', result);
+    } catch (error) {
+      console.error('Error handling payment:', error);
+    }
+  };
+
+
+
+  // we will be filling this object in next step.
+  const options = {
+    key: keyId, // key id from props
+    amount, // Amount in lowest denomination from props
+    currency, // Currency from props.
+    name: 'Dukaan', // Title for your organization to display in checkout modal
+    // image, // custom logo  url
+    order_id: orderId, // order id from props
+    // This handler menthod is always executed in case of succeeded payment
+    handler: (response) => {
+      console.log('succeeded');
+      console.log(response);
+      paymentId.current = response.razorpay_payment_id;
+
+      // Most important step to capture and authorize the payment. This can be done of Backend server.
+      const succeeded = crypto.HmacSHA256(`${orderId}|${response.razorpay_payment_id}`, keySecret).toString() === response.razorpay_signature;
+
+      // If successfully authorized. Then we can consider the payment as successful.
+      if (succeeded) {
+        handlePayment('succeeded', {
+          orderId,
+          paymentId,
+          signature: response.razorpay_signature,
+        });
+      } else {
+        handlePayment('failed', {
+          orderId,
+          paymentId: response.razorpay_payment_id,
+        });
+      }
+    },
+    callback_url : `http://localhost:3000/order-success/${currentOrder.id}`,
+    modal: {
+      confirm_close: true, // this is set to true, if we want confirmation when clicked on cross button.
+      // This function is executed when checkout modal is closed
+      // There can be 3 reasons when this modal is closed.
+      ondismiss: async (reason) => {
+        const {
+          reason: paymentReason, field, step, code,
+        } = reason && reason.error ? reason.error : {};
+        // Reason 1 - when payment is cancelled. It can happend when we click cross icon or cancel any payment explicitly. 
+        if (reason === undefined) {
+          console.log('cancelled');
+          handlePayment('Cancelled');
+        } 
+        // Reason 2 - When modal is auto closed because of time out
+        else if (reason === 'timeout') {
+          console.log('timedout');
+          handlePayment('timedout');
+        } 
+        // Reason 3 - When payment gets failed.
+        else {
+          console.log('failed');
+          handlePayment('failed', {
+            paymentReason, field, step, code,
+          });
+        }
+      },
+    },
+    // This property allows to enble/disable retries.
+    // This is enabled true by default. 
+    retry: {
+      enabled: false,
+    },
+    timeout: 900, // Time limit in Seconds
+    theme: {
+      color: '', // Custom color for your checkout modal.
+    },
+  };
+  useEffect(() => {
+    console.log('in razorpay');
+    displayRazorpay(options);
+  }, []);
+
+  return null;
+};
+
+export default RenderRazorpay;
--- src/pages/StripeCheckout.js
+++ src/pages/StripeCheckout.js
@@ -1,47 +1,75 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import CheckoutForm from "./CheckoutForm";
+import RenderRazorpay from "./CheckoutForm";
 import "../Stripe.css";
 import { selectCurrentOrder } from "../features/order/orderSlice";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_51N5NLVSF2Mo4AGVvYGMMNnTcPA5lHrkUv8zPymsKfL31c8m6et8525Y92zMgA22m1u6ZkkXsBofre0PJL6i3eVsh00sR6M8c00");
 
 export default function StripeCheckout() {
-  const [clientSecret, setClientSecret] = useState("");
-  const currentOrder = useSelector(selectCurrentOrder)
-
+  const currentOrder = useSelector(selectCurrentOrder);
+  const [displayRazorpay, setDisplayRazorpay] = useState(false);
+  const [orderDetails, setOrderDetails] = useState({
+    orderId: null,
+    currency: null,
+    amount: null,
+  });
   useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
     fetch("/create-payment-intent", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ totalAmount: currentOrder.totalAmount, orderId:currentOrder.id }),
-    
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: currentOrder.totalAmount, // convert amount into lowest unit (Dollar -> Cents)
+        orderId: currentOrder.id,
+        keyId: "rzp_test_h27cx8k0njEGHh",
+        KeySecret: "KY6HyMf6wbE5dNKXUxEqgjOK",
+      }),
     })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
+      .then((response) => response.json()) // Convert the response to JSON
+      .then((data) => {
+        if (data && data.order_id) {
+          setOrderDetails({
+            orderId: data.order_id,
+            currency: data.currency,
+            amount: data.amount,
+          });
+          setDisplayRazorpay(true);
+        }
+      })
+      .catch((error) => {
+        console.error("Error creating order:", error);
+      });
   }, []);
 
-  const appearance = {
-    theme: 'stripe',
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
+  // const appearance = {
+  //   theme: 'stripe',
+  // };
+  // const options = {
+  //   clientSecret,
+  //   appearance,
+  // };
 
   return (
     <div className="Stripe">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
+      {displayRazorpay && (
+        <>
+        <h1>aaaaaaaa</h1>
+        <RenderRazorpay
+        
+          amount={orderDetails.amount}
+          currency={orderDetails.currency}
+          orderId={orderDetails.orderId}
+          keyId={process.env.REACT_APP_RAZORPAY_KEY_ID}
+          keySecret={process.env.REACT_APP_RAZORPAY_KEY_SECRET}
+        />
+        </>
       )}
     </div>
   );
